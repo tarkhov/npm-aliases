bdfc782760c580e43a269e606a3fcc21918d2199
tarkhov <agtuks@gmail.com>
2019-06-15 14:44:20 +0300

    First commit

---
+++ etc/profile.d/npm-aliases.sh
@@ -0,0 +1,23 @@
+alias n='npm'
+alias ni='npm install'
+alias nid='npm install --save-dev'
+alias nig="npm install -g"
+alias nu='npm uninstall'
+alias nr='npm run'
+
+# Vue, Nuxt, React, Next
+alias nrb='npm run build'
+
+# Vue
+alias nrl='npm run lint'
+alias nrsr='npm run serve'
+alias nrt='npm run test'
+
+# Nuxt
+alias nrd='npm run dev'
+alias nrg='npm run generate'
+alias nrst='npm run start'
+
+# Next
+alias ns='npm start'
+alias nt='npm test'
